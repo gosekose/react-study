d870d79fd6bc86415dbae6f9c216ef05391f1b6b
gosekose <ksyn1611@naver.com>
2024-07-09 19:04:07 +0900

    prop types

--- movie/movie-app/src/components/Card.js
+++ movie/movie-app/src/components/Card.js
@@ -1,9 +1,12 @@
-const Card = ({ title, body }) => {
+const Card = ({ title, body, children }) => {
     return (
         <div className="card mb-3">
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
-                <p className="card-text">{body}</p>
+                <div className="d-flex justify-content-between">
+                    <p className="card-text">{body}</p>
+                    {children && <div>{children}</div>}
+                </div>
             </div>
         </div>
     );
--- movie/movie-app/src/pages/ListPage.js
+++ movie/movie-app/src/pages/ListPage.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Card from "../components/Card";
+import PropTypes from 'prop-types';
+
 
 const ListPage = () => {
     const [posts, setPosts] = useState([]);
@@ -21,12 +23,19 @@ const ListPage = () => {
             <h1>Blogs</h1>
             {posts.map(post => {
                 return (
-                    <Card key={post.id} title={post.title} body={post.body}/>
+                    <Card key={post.id} title={post.title} body={post.body}>
+                        <button>이동</button>
+                    </Card>
                 )
             }
             )}
         </div>
     );
+}
+
+Card.prototypes = {
+    title: PropTypes.string.isRequired,
+    body: PropTypes.string,
 }
 
 export default ListPage
