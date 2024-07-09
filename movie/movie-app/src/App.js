5aa2d54a725616ffa46d57a70a33e9541ca8083e
gosekose <ksyn1611@naver.com>
2024-07-09 17:25:10 +0900

    React Router 사용

--- movie/movie-app/src/App.js
+++ movie/movie-app/src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axois from 'axios';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const onSubmit = () => {
+    console.log(title, body);
+    axois.post('http://localhost:3030/posts', {
+      title: title,
+      body: body
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <div className="mb-3">
+      </div>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link class="navbar-brand" to="/">Home</Link>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <Link class="nav-link" to="/blogs">Blogs</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" exact element={
+          <div>
+            Home Page
+          </div>
+        }></Route>
+        <Route 
+        path="/blogs"
+        exact
+        element={
+          <div className="container">
+            <h1>Create Blog Post</h1>
+            <div className='mb-3'>
+            <label className='form-label'>Title</label>
+            <input 
+              className="form-control"
+              value={title}
+              onChange={ (event) => {
+                setTitle(event.target.value)
+                console.log(event.target.value)
+              }}
+              />
+            </div>
+            <div className='mb-3'>
+              <label className='form-label'>Body</label>
+              <textarea 
+              className="form-control"
+              value={body}
+              onChange={ (event) => {
+                setBody(event.target.value)
+                console.log(event.target.value)
+              }}
+              rows='20'
+              />
+            </div>
+            <button 
+            className='btn btn-primary'
+            onClick={ onSubmit }
+            >
+              Post
+              </button>
+          </div>
+        }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
